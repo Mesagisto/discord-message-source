9f61bb9a4449ff2849528ded4841c0317340aa8e
itsusinn <itsusinn@foxmail.com>
2022-01-21 00:11:43 +0800

    ci: 使用docker进行交叉编译

--- .github/before-build-linux.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install -y libopus-dev
-sudo apt install -y musl-tools
-sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/x86_64-linux-musl/asm
-sudo ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/asm-generic
-sudo ln -s /usr/include/linux /usr/include/x86_64-linux-musl/linux
-rustup target add x86_64-unknown-linux-musl
