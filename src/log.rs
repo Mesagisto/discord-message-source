de97e4914669c50331824c1997165ff16191de9e
Itsusinn <itsusinn@meowcat.org>
2022-07-14 11:49:27 +0800

    feat: support tokio-console

---
+++ src/log.rs
@@ -0,0 +1,40 @@
+use chrono::{Local, Offset, TimeZone};
+use tracing::Level;
+use tracing_subscriber::prelude::*;
+
+pub(crate) fn init()  {
+  #[cfg(not(feature = "tokio-console"))]
+  let filter = tracing_subscriber::filter::Targets::new()
+    .with_target("serenity", Level::INFO)
+    .with_target("discord_message_source", Level::INFO)
+    .with_target("mesagisto_client", Level::TRACE)
+    .with_default(Level::WARN);
+  #[cfg(feature = "tokio-console")]
+  let filter = tracing_subscriber::filter::Targets::new()
+    .with_target("serenity", Level::INFO)
+    .with_target("discord_message_source", Level::INFO)
+    .with_target("mesagisto_client", Level::TRACE)
+    .with_target("tokio",Level::TRACE)
+    .with_target("runtime",Level::TRACE)
+    .with_default(Level::WARN);
+  let registry = tracing_subscriber::registry()
+    .with(
+      tracing_subscriber::fmt::layer()
+        .with_target(true)
+        .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
+          time::UtcOffset::from_whole_seconds(
+            Local.timestamp(0, 0).offset().fix().local_minus_utc(),
+          )
+          .unwrap_or(time::UtcOffset::UTC),
+          time::macros::format_description!(
+            "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
+          ),
+        )),
+    )
+    .with(filter);
+
+  #[cfg(feature = "tokio-console")]
+  registry.with(console_subscriber::spawn()).init();
+  #[cfg(not(feature = "tokio-console"))]
+  registry.init();
+}
